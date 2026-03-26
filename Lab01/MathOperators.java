daf6a7a2d0105f03c7ed5a34f66c310b5a23cef7
HaimoihocIT <daihai288@gmail.com>
2026-03-26 13:54:18 +0700

    Complete assignment 2.2.5

---
+++ Lab01/MathOperators.java
@@ -0,0 +1,28 @@
+package Lab01;
+
+import javax.swing.JOptionPane;
+
+public class MathOperators {
+    public static void main(String[] args) {
+        String strNum1 = JOptionPane.showInputDialog(null,"Enter the first number:","Input 1st Num",JOptionPane.INFORMATION_MESSAGE);
+        String strNum2 = JOptionPane.showInputDialog(null,"Enter the second number:","Input 2nd Num",JOptionPane.INFORMATION_MESSAGE);
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        
+        String error;
+        if(num2 != 0){
+            double quotient = num1/num2;
+            error = "quotient: " + quotient;
+        }else {
+                error = "can't divide by 0!";
+            }
+            String result = "Sum: " + sum + "\n" + "Difference: " + difference + "\n" + "Product: " + product + "\n" + error;
+            JOptionPane.showMessageDialog(null, result, "Result",JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+    }
+}
